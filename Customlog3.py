9836ab8a49b214ad8f1692c07ed0d781aa12463d
Suraj Singh <36315541+mannusillegal@users.noreply.github.com>
2023-02-19 03:02:05 +0530

    Update Customlog3.py

--- Customlog3.py
+++ Customlog3.py
@@ -1,34 +1,48 @@
-import subprocess
-import os
 import boto3
+import os
+import subprocess
+import time
 
-# AWS CloudWatch log group name
+# Constants
 LOG_GROUP_NAME = 'my-log-group'
-
-# Docker container names and log categories
-CONTAINERS = {
-    'my-app': 'app-logs',
-    'my-database': 'db-logs'
-}
-
-# Temp log file directory
+LOG_CATEGORY = 'my-log-category'
 TEMP_LOG_DIR = '/app/tmp'
 
 # AWS CloudWatch client
 cloudwatch = boto3.client('logs')
 
-def create_log_group_if_not_exists(log_group_name):
-    # Check if the log group exists, create it if it does not exist
-    response = cloudwatch.describe_log_groups(logGroupNamePrefix=log_group_name)
+def create_log_group(log_group_name):
+    # Check if log group exists
+    response = cloudwatch.describe_log_groups(
+        logGroupNamePrefix=log_group_name
+    )
+
     if len(response['logGroups']) == 0:
         # Log group does not exist, create it
-        cloudwatch.create_log_group(logGroupName=log_group_name)
-        print(f'Log group "{log_group_name}" created successfully.')
+        cloudwatch.create_log_group(
+            logGroupName=log_group_name
+        )
+        print(f'Log group {log_group_name} created.')
     else:
         # Log group already exists
-        print(f'Log group "{log_group_name}" already exists.')
+        print(f'Log group {log_group_name} exists.')
 
 def upload_logs_to_cloudwatch(log_group_name, container_name, log_category):
+    # Get log stream name
+    log_stream_name = container_name
+
+    # Check if log stream exists, create it if it does not exist
+    response = cloudwatch.describe_log_streams(
+        logGroupName=log_group_name,
+        logStreamNamePrefix=log_stream_name
+    )
+    if len(response['logStreams']) == 0:
+        # Log stream does not exist, create it
+        cloudwatch.create_log_stream(
+            logGroupName=log_group_name,
+            logStreamName=log_stream_name
+        )
+
     # Get log file path
     log_file = f'{TEMP_LOG_DIR}/{container_name}.log'
 
@@ -41,7 +55,7 @@ def upload_logs_to_cloudwatch(log_group_name, container_name, log_category):
         # Upload logs to CloudWatch
         cloudwatch.put_log_events(
             logGroupName=log_group_name,
-            logStreamName=container_name,
+            logStreamName=log_stream_name,
             logEvents=[
                 {
                     'timestamp': int(round(time.time() * 1000)),
@@ -53,25 +67,30 @@ def upload_logs_to_cloudwatch(log_group_name, container_name, log_category):
         # Delete log file
         os.remove(log_file)
 
-def pull_container_logs(container_name, log_category):
-    # Set log file path
-    log_file = f'{TEMP_LOG_DIR}/{container_name}.log'
+def get_container_names():
+    # Get container names
+    cmd = "docker ps --format '{{.Names}}'"
+    output = subprocess.check_output(cmd, shell=True)
+    container_names = output.decode().strip().split('\n')
+    return container_names
 
-    # Pull logs from Docker container
-    subprocess.run(['docker', 'logs', container_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    # Write logs to file
-    with open(log_file, 'w') as f:
-        f.write(log_data)
+def pull_docker_logs(container_names):
+    # Pull logs from each container
+    for container_name in container_names:
+        cmd = f'docker logs {container_name} > {TEMP_LOG_DIR}/{container_name}.log'
+        os.system(cmd)
 
 def main():
-    # Check if log group exists, create it if it does not exist
-    create_log_group_if_not_exists(LOG_GROUP_NAME)
+    # Create log group if it does not exist
+    create_log_group(LOG_GROUP_NAME)
 
-    # Pull logs from each container and upload to CloudWatch
-    for container_name, log_category in CONTAINERS.items():
-        pull_container_logs(container_name, log_category)
-        upload_logs_to_cloudwatch(LOG_GROUP_NAME, container_name, log_category)
+    # Get container names
+    container_names = get_container_names()
+
+    # Pull logs from containers and upload to AWS CloudWatch
+    for container_name in container_names:
+        pull_docker_logs([container_name])
+        upload_logs_to_cloudwatch(LOG_GROUP_NAME, container_name, LOG_CATEGORY)
 
 if __name__ == '__main__':
     main()
